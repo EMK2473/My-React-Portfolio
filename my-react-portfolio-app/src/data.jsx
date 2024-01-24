fe4950804fe2b42188339367bd7b48c8df610cc3
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-01-24 12:26:29 -0500

    added data.jsx file and dev'd app.jsx components

---
+++ my-react-portfolio-app/src/data.jsx
@@ -0,0 +1,121 @@
+export const projects = [
+  // {
+  //   title: "JavaScript Quiz",
+  //   subtitle: "JavaScript Quiz",
+  //   description:
+  //     "A quiz designed to test your knowledge of JavaScript",
+  //   image: "./jsquiz.gif",
+  //   link: "https://emk2473.github.io/javascript-quiz/",
+  // },
+  // {
+  //   title: "Daily Planner",
+  //   subtitle: "Sundial Daily Planner",
+  //   description:
+  //     "A Daily 9-5 Planner to help manage your time effectively.",
+  //   image: "./planner.gif",
+  //   link: "https://emk2473.github.io/daily-planner/",
+  // },
+  // {
+  //   title: "5-Day Weather Forecast",
+  //   subtitle: "5-Day Weather Forecast",
+  //   description:
+  //     "A 5-Day Weather Forecast Application.",
+  //   image: "./5dayGIF.gif",
+  //   link: "https://emk2473.github.io/weather-forecast/",
+  // },
+  {
+    title: "MVC Tech Blog",
+    subtitle: "Online Blog Site",
+    description:
+      "A Tech Blog Site using the Model, Controller, and View framework.",
+    image: "./techBlog.gif",
+    link: "https://mvc-tech-blogsite-dad8725474ac.herokuapp.com/",
+  },
+  {
+    title: "Inventory Management System",
+    subtitle: "IMS",
+    description:
+      "An inventory management database for companies to manage their inventory through users providing live an up-to-date inventory information.",
+    image: "./invWarMan.gif",
+    link: "https://inv-man-system-b9da5a82b52b.herokuapp.com/login",
+  },
+  {
+    title: "Note Taker",
+    subtitle: "Take notes with Express",
+    description: "An Application for writing and saving notes using a server.",
+    image: "./expNoteGIF.gif",
+    link: "https://exp-note-taker-app-7afdb91c4244.herokuapp.com/",
+  },
+  {
+    title: "J.A.T.E.",
+    subtitle: "Just Another Text Editor",
+    description:
+      "A text editor that runs in the browser. Create notes or code snippets with or without an internet connection so you can reliably retrieve them for later use!",
+    image: "./jate.gif",
+    link: "https://j-a-t-e-867088540b22.herokuapp.com/",
+  },
+  {
+    title: "Calliope's Commonplace",
+    subtitle: "An App designed to embody Calliope",
+    description:
+      "A Group Project designed to introduce API's and CSS Frameworks.",
+    image: "./callCommGIF.gif",
+    link: "https://emk2473.github.io/calliope-commonplace/",
+  },
+  {
+    title: "word.",
+    subtitle: "word.",
+    description: "A fun project to practice with React, Tailwind, and RNG.",
+    image: "./word.gif",
+    link: "https://darling-chebakia-cb0515.netlify.app/",
+  },
+  {
+    title: "Professional ReadMe Generator",
+    subtitle: "Readme.md Generator",
+    description:
+      "A web application designed to quickly generate a professional README.md file.",
+    image: "./readmeGenerator.gif",
+    link: "https://superb-dragon-e1dd77.netlify.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+    `"I strongly recommend Eric for their exceptional mental effort and consistent performance throughout the coding bootcamp."`,
+    image: "https://randomuser.me/api/portraits/men/21.jpg",
+    name: "Jamie DeLong (hopefully)",
+    company: "Instructor",
+  },
+  {
+    quote:
+      `"I endorse Eric's candidacy for employment, aiming to gain firsthand insights into their professional performance, facilitating an informed decision for potential future collaboration."`,
+    image: "https://randomuser.me/api/portraits/men/72.jpg",
+    name: "Eliot Cleveland (hopefully)",
+    company: "Teaching Assistant",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "Express",
+  "MySQL",
+  "NoSQL",
+  "GraphQL",
+  "Material UI",
+  "HTML",
+  "CSS",
+  "Git",
+  "XML",
+  "SVG",
+];
+
+export const working = [
+  "VCU Engineering Program",
+  "Master SCRUM Certification",
+  "Python Certification",
+  "FreeCodeCamp.com Certification",
+];
