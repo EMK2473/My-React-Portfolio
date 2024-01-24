bb013175009ed1f4ad393d3e8559546e85522cfa
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-01-24 14:48:33 -0500

    generateResumePDF added

--- src/generateResumePDF.jsx
+++ src/generateResumePDF.jsx
@@ -1,15 +1,8 @@
-// pdfUtils.js
 import jsPDF from "jspdf";
 
-// Utility function to generate and save PDF
 const generateAndSavePDF = () => {
-  // Create a new jsPDF instance
   const pdf = new jsPDF();
-
-  // Add the content to the PDF
   pdf.text("This is going to be my resume!", 10, 10);
-
-  // Save the PDF as a file
   pdf.save("MyResume.pdf");
 };
 
