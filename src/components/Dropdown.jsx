cc7a5203f2cb0502201ec7dbc21dcb68dc459ba9
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-01-24 15:33:52 -0500

    dropdown edits

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -24,17 +24,11 @@ const MultiSelectDropdown = () => {
     { key: 'jquery', value: 'jquery', text: 'jQuery' },
     { key: 'apis', value: 'apis', text: 'APIs' },
     { key: 'ajax', value: 'ajax', text: 'AJAX' },
-    { key: 'agilemethodologies', value: 'agilemethodologies', text: 'Agile Methodologies (Scrum, Kanban, SAFe, etc.)' },
     { key: 'projectmanagement', value: 'projectmanagement', text: 'Project Management' },
-    { key: 'crossfunctionalteamfacilitation', value: 'crossfunctionalteamfacilitation', text: 'Cross-Functional Team Facilitation' },
-    { key: 'scrumofscrums', value: 'scrumofscrums', text: 'Scrum of Scrums (SoS)' },
     { key: 'sprintplanningandretrospectivemeetings', value: 'sprintplanningandretrospectivemeetings', text: 'Sprint Planning and Retrospective Meetings' },
     { key: 'continuousimprovement', value: 'continuousimprovement', text: 'Continuous Improvement' },
     { key: 'softwaredevelopment', value: 'softwaredevelopment', text: 'Software Development' },
-    { key: 'negotiationandriskreduction', value: 'negotiationandriskreduction', text: 'Negotiation and Risk Reduction' },
-    { key: 'metricsprogramimplementation', value: 'metricsprogramimplementation', text: 'Metrics Program Implementation' },
     { key: 'teambuilding', value: 'teambuilding', text: 'Team Building' },
-    { key: 'communicationandfacilitation', value: 'communicationandfacilitation', text: 'Communication and Facilitation' },
     { key: 'conflictresolution', value: 'conflictresolution', text: 'Conflict Resolution' },
     { key: 'problemsolving', value: 'problemsolving', text: 'Problem-Solving' },
     { key: 'timemanagement', value: 'timemanagement', text: 'Time Management' },
@@ -42,7 +36,6 @@ const MultiSelectDropdown = () => {
     { key: 'coachingandmentorship', value: 'coachingandmentorship', text: 'Coaching and Mentorship' },
     { key: 'processimprovement', value: 'processimprovement', text: 'Process Improvement' },
     { key: 'qualityassurance', value: 'qualityassurance', text: 'Quality Assurance' },
-    { key: 'opensourcetechnologies', value: 'opensourcetechnologies', text: 'Open Source Technologies' },
   ];
   
       
