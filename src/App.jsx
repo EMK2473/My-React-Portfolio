7b52bd68cab56e4936f9224f062478b7d7398b3c
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-01-24 14:00:24 -0500

    dropdown edits

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
 import Working from "./components/Working"
-import Dropdown from "./components/Dropdown"
+
 
 function App() {
   return (
@@ -15,7 +15,6 @@ function App() {
       <About />
       <Projects />
       <Skills />
-      <Dropdown />
       <Working />
       <Testimonials />
       <Contact />
